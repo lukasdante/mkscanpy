7b41b172c07f83b08a54c156f32f37e40bdb7d8d
vboxuser <vboxuser@u18.myguest.virtualbox.org>
2024-10-31 09:48:39 +0800

    first commit

---
+++ mksCAN.py
@@ -0,0 +1,245 @@
+import can
+import time
+
+class Motor:
+    def __init__(self, name, bus, version, can_id, mode='serial-vfoc', set_defaults=True, *args, **kwargs):
+        self.name = name
+        self.bus = bus
+        self.version = self.set_version(version)
+        self.can_id = self.set_can_id(can_id)
+        self.mode = self.set_mode(mode)
+
+        if set_defaults:
+            set_defaults()
+            
+    def set_defaults(self):
+        
+        pass
+
+
+    def add_crc(data: list):
+        crc = sum(data)
+        crc = 0xFF & crc
+        return data.append(crc)
+
+    def calibrate(self):
+        data = [self.can_id, 0x80, 00]
+        return can.Message(dlc=len(data), data=self.add_crc(data))
+
+    def set_version(self, version):
+        VERSIONS = {
+            '42D': [1600, 3000, 800],
+            '57D': [3200, 5200, 400],
+            '28D': [600,  3000, 200],
+            '35D': [800,  3000, 200],
+        }
+
+        if version not in VERSIONS.keys():
+            raise ValueError(f'The only MKS Servo versions supported are {", ".join(VERSIONS)}')
+
+        self.version = version
+        self.ma = VERSIONS[version[0]]
+        self.max_ma = VERSIONS[version[1]]
+        self.home_ma = VERSIONS[version[2]]
+
+    def set_can_id(self, can_id):
+        if 0x00 < can_id < 0x800: 
+            if self.can_id:
+                can.Message(data=[self.can_id, ])
+            self.can_id = can_id
+        else:
+            raise ValueError('The CAN ID must be between 0 and 2047 or 0x800.')
+
+    def set_mode(self, mode):
+        MODES = [
+            'pulse-open',
+            'pulse-close',
+            'pulse-vfoc',
+            'serial-open',
+            'serial-close',
+            'serial-vfoc',
+        ]
+
+        if mode not in MODES:
+            raise ValueError(f'The only supported modes are {", ".join(MODES)}')
+
+        if 'open' in mode:
+            self.max_rpm = 400
+        if 'close' in mode:
+            self.max_rpm = 1500
+        if 'vfoc' in mode:
+            self.max_rpm = 3000
+            
+
+    def read_params(self, mode='addition'):
+        MODES = [
+            'addition',
+            'carry',
+        ]
+        if mode not in MODES:
+            raise ValueError(f'The only allowed modes are {", ".join(MODES)}')
+
+        pass
+
+    def set_working_current(self, ma):
+        if ma > self.max_ma:
+            raise ValueError(f'The MKS Servo{self.version} can only have a maximum working current of {self.max_ma} mA.')
+        
+        self.ma = ma
+
+    def set_holding_current(self, hold_ma):
+        if 'vfoc' in self.mode:
+            raise TypeError(f'Cannot set holding current for {self.mode} mode.')
+        
+        self.hold_ma = hold_ma
+
+    def set_microstepping(self, mstep):
+        if 0 > mstep > 256:
+            raise ValueError('Microstepping is only allowed between 1-256 subdivisions.')
+        
+        self.mstep = mstep
+
+    def set_en_level(self, en_level):
+        EN_LEVELS = [
+            'high', 'low', 'hold'
+        ]
+
+        if en_level not in EN_LEVELS:
+            raise ValueError(f'The only EN levels supported are {", ".join(EN_LEVELS)}.')
+        
+        self.set_en_level = en_level
+
+    # possible to add wrapper instead of f(x)
+    def _dir(dir: str, variable: str):
+        DIRS = ['CCW', 'CW']
+
+        if dir not in DIRS:
+            raise ValueError(f'The only {variable} supported are {", ".join(DIRS)}')
+
+
+    # for pulse interface
+    def set_motor_direction(self, dir):
+
+        if "pulse" not in self.mode:
+            raise ValueError("Cannot set motor direction for pulse interface mode.")
+
+        self._dir(dir, "motor_directions")
+            
+        self.motor_direction = dir
+
+    def set_screen_auto_turnoff(self, auto: bool):
+        self.screen_auto_turnoff = auto
+
+    def is_oled_auto_turnoff(self):
+        return self.screen_auto_turnoff
+
+    def set_locked_rotor_protection(self, protect: bool):
+        self.locked_rotor_protection = protect
+
+    def is_locked_rotor_protection(self):
+        return self.locked_rotor_protection
+
+    def set_mplyer(self, mplyer: bool):
+        self.mplyer = mplyer
+
+    def is_mplyer_enabled(self):
+        return self.mplyer
+
+    def set_can_rate(self, can_rate):
+        CAN_RATES = [
+            125e3, 250e3, 500e3, 1e6
+        ]
+
+        if can_rate not in CAN_RATES:
+            raise ValueError(f'The only CAN rates supported are {", ".join(CAN_RATES)}.')
+
+    def set_can_id(self, can_id):
+        if 0x00 < can_id < 0x800: 
+            self.can_id = can_id
+        else:
+            raise ValueError('The CAN ID must be between 0 and 2047.')
+
+    def set_can_rsp(self, can_rsp: bool):
+        self.can_rsp = can_rsp
+
+    def is_can_rsp_enabled(self):
+        return self.can_rsp
+
+    def set_zero_mode(self, mode):
+        MODES = [
+            'disable', 'direction', 'near'
+        ]
+
+        if mode not in MODES:
+            raise ValueError(f'The mode only supported are {", ".join(MODES)}.')
+        
+        self.zero_mode = mode
+
+    def set_zero(self):
+        if self.zero_mode == 'disable':
+            raise PermissionError('Cannot set zero position if zero mode is disabled.')
+        pass
+
+    def set_zero_speed(self, speed: int):
+        if speed in range(0, 5):
+            self.zero_speed = speed
+            return
+        
+        raise ValueError('Speed must be set between 0-4.')
+
+    def set_zero_direction(self, dir):
+        self._dir(dir, "zero directions")
+
+        self.zero_direction = dir
+
+
+    def set_home_trigger_high(self, trigger: bool):
+        self.home_trigger = trigger
+        pass
+
+    def is_home_trigger_high(self):
+        return self.home_trigger
+
+    def set_home_direction(self, dir):
+        self._dir(dir, "home directions")
+        
+        self.home_direction = dir
+
+    def use_home_limit_switch(self, use_limit_switch: bool):
+        self.home_limit_switch = use_limit_switch
+
+    def is_home_limit_switch_enabled(self):
+        return self.home_limit_switch
+
+    def set_home_current(self, ma):
+        if not self.is_home_limit_switch_enabled():
+            raise PermissionError('Cannot adjust home current if no home limit switch is used.')
+        self.home_ma = ma
+
+    def go_home(self):
+        pass
+
+    def set_end_limit(self, end_limit: bool):
+        if not self.end_limit:
+            self.go_home()
+        self.end_limit = end_limit
+
+    def is_end_limit_enabled(self):
+        return self.end_limit
+
+    def restore(self):
+        self.calibrate()
+        pass
+
+    def show_params(self):
+        for attr in dir(self):
+            if not callable(getattr(self, attr)) and not attr.startswith("__"):
+                print(attr, ":", getattr(self, attr))
+
+
+    # Commands
+
+    def turn(self, amount, direction, velocity, acceleration, type='absolute'):
+        data = bytearray([self.can_id, ])
+        msg = can.Message()
+
